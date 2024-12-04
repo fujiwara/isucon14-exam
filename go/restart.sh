327d1132750e2de823bd75db08fa18d324bd79d2
fujiwara <fujiwara.shunichiro@gmail.com>
2024-12-04 15:24:25 +0000

    multi hosts

--- go/restart.sh
+++ go/restart.sh
@@ -2,10 +2,17 @@
 
 set -eux
 make
-sudo rm -f /var/log/nginx/access.log
-sudo systemctl restart nginx
-sudo systemctl restart isuride-go.service
-sudo chmod 644 /var/log/nginx/access.log
+
+for host in 192.168.0.12 192.168.0.13; do
+    rsync -av ~/env.sh $host:/home/isucon/env.sh
+    sudo rsync -av --delete --exclude .others ../ $host:/home/isucon/webapp/
+    sudo rsync -av /etc/nginx/ $host:/etc/nginx/
+done
+
+for host in 192.168.0.11 192.168.0.12 192.168.0.13; do
+    ssh $host 'sudo rm -f /var/log/nginx/access.log; sudo systemctl restart nginx; sudo systemctl restart isuride-go.service'
+done
+
+# localhost
 sudo truncate --size 0 /var/log/mysql/mysql-slow.log
 mysqladmin -uisucon -pisucon flush-logs
-sudo journalctl -u isuride-go.service -f
