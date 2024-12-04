d1c4bfdea0df2e25ba03daeba844316ca7eac964
fujiwara <fujiwara.shunichiro@gmail.com>
2024-12-04 05:09:56 +0000

    initial

---
+++ go/middlewares.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"net/http"
+)
+
+func appAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c, err := r.Cookie("app_session")
+		if errors.Is(err, http.ErrNoCookie) || c.Value == "" {
+			writeError(w, http.StatusUnauthorized, errors.New("app_session cookie is required"))
+			return
+		}
+		accessToken := c.Value
+		user := &User{}
+		err = db.Get(user, "SELECT * FROM users WHERE access_token = ?", accessToken)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				writeError(w, http.StatusUnauthorized, errors.New("invalid access token"))
+				return
+			}
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func ownerAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c, err := r.Cookie("owner_session")
+		if errors.Is(err, http.ErrNoCookie) || c.Value == "" {
+			writeError(w, http.StatusUnauthorized, errors.New("owner_session cookie is required"))
+			return
+		}
+		accessToken := c.Value
+		owner := &Owner{}
+		if err := db.Get(owner, "SELECT * FROM owners WHERE access_token = ?", accessToken); err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				writeError(w, http.StatusUnauthorized, errors.New("invalid access token"))
+				return
+			}
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "owner", owner)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func chairAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c, err := r.Cookie("chair_session")
+		if errors.Is(err, http.ErrNoCookie) || c.Value == "" {
+			writeError(w, http.StatusUnauthorized, errors.New("chair_session cookie is required"))
+			return
+		}
+		accessToken := c.Value
+		chair := &Chair{}
+		err = db.Get(chair, "SELECT * FROM chairs WHERE access_token = ?", accessToken)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				writeError(w, http.StatusUnauthorized, errors.New("invalid access token"))
+				return
+			}
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "chair", chair)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
